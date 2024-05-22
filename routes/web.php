5e2daf7b7d3cd95d7a0b44acbe0df84f7110a6f5
thirsty-wing <112518380+thirsty-wing@users.noreply.github.com>
2024-05-21 22:03:02 -0600

    day 6: implement jobs page and job page
    dd() is a function that means "dump and die". You can pass things to it and it
    will print them out and stop execution.
    
    Laravel's Arr class contains many utility functions.
    
    regular php functions do not capture their surroundings. In order to reference
    a variable outside of a function definition, the `use` keyword must be used.
    Example:
    
    ```
    $id = 5;
    
    function jobHasId($job) use ($id) {
      return $job['id'] == $id;
    });
    ```
    
    arrow functions, on the other hand, do capture surroundings.
    
    ```
    $id = 5;
    
    $jobHasIdFn = fn($job) => $job['id'] == $id;
    ```
    
    This is called capture-by-value.

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,56 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+            [
+                'id' => 1,
+                'salary' => '$50,000',
+                'title' => 'Director',
+            ],
+            [
+                'id' => 2,
+                'salary' => '$10,000',
+                'title' => 'Programmer',
+            ],
+            [
+                'id' => 3,
+                'salary' => '$40,000',
+                'title' => 'Teacher',
+            ],
+        ],
+    ]);
+});
+
+Route::get('/jobs/{id}', function ($id) {
+    $jobs = [
+        [
+            'id' => 1,
+            'salary' => '$50,000',
+            'title' => 'Director',
+        ],
+        [
+            'id' => 2,
+            'salary' => '$10,000',
+            'title' => 'Programmer',
+        ],
+        [
+            'id' => 3,
+            'salary' => '$40,000',
+            'title' => 'Teacher',
+        ],
+    ];
+
+    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+
+    return view('job', [ 'job' => $job ]);
 });
 
 Route::get('/contact', function () {
