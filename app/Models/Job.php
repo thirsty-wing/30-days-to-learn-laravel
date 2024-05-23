d90a56af65ec3ddf8a7743b10625abb0adb5f4f9
thirsty-wing <112518380+thirsty-wing@users.noreply.github.com>
2024-05-22 22:31:30 -0600

    day 11: define functions in models for table relationships
    a function can be created in a model defining a "belongsTo" relationship. Think
    $job->employer.
    
    Also a function can be created defining a "hasMany" relationship. Think
    $employer->jobs.

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model
@@ -15,4 +14,8 @@ class Job extends Model
     protected $table = "job_listings";
 
     protected $fillable = ["title", "salary"];
+
+    public function employer() {
+        return $this->belongsTo(Employer::class);
+    }
 }
