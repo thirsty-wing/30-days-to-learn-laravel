fb019433df569c626f0846177e402364ec4fa107
thirsty-wing <112518380+thirsty-wing@users.noreply.github.com>
2024-05-27 16:01:26 -0600

    day 16: do a form post
    The sequence of declared routes matters. Wildcards should be declared after
    other things.
    
    There are naming conventions for breaking up the views directory into
    additional directories; index is for the base route, show is for a specific one
    of that base route (think "/jobs/123").
    
    The convention for a creation form is to have a form at "/jobs/create" for
    example, and from there do a form POST to action="/jobs".
    
    Laravel throws a 419 error response when a form is submitted (at least with
    POST method) due to danger of cross site request forgery (CSRF). In Laravel
    blade we can use the @csrf directive to take care of adding a hidden element
    with the CSRF token.
    
    fillable fields protection can be disabled by setting an empty guarded field in the model.

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -13,13 +13,15 @@ class Job extends Model
 
     protected $table = "job_listings";
 
-    protected $fillable = ["title", "salary"];
+    protected $guarded = [];
 
-    public function employer() {
+    public function employer()
+    {
         return $this->belongsTo(Employer::class);
     }
 
-    public function tags() {
+    public function tags()
+    {
         return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listing_id");
     }
 }
