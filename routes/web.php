d1338cd411ec628bccde899827c28d446cba8add
thirsty-wing <112518380+thirsty-wing@users.noreply.github.com>
2024-05-28 22:41:42 -0600

    Day 18: implement PATCHing and DELETEing a resource
    Laravel can change the method of a form to patch by using the
    `@method('PATCH')` directive.
    
    You can use a button to trigger a hidden form if there is a button in the
    visible form that needs to do something else.

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,39 @@ Route::post("/jobs", function () {
     return redirect('/jobs');
 });
 
+Route::get("/jobs/{id}/edit", function ($id) {
+    $job = Job::find($id);
+
+    return view("jobs.edit", ["job" => $job]);
+});
+
+Route::patch("/jobs/{id}", function ($id) {
+    // validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' =>  ['required'],
+    ]);
+    // authorize (On hold...)
+    // update the job and persist
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    // redirect to the job page
+    return redirect('/jobs/' . $job->id);
+});
+
+Route::delete("/jobs/{id}", function ($id) {
+    // authorize (On hold...)
+    // delete the job
+    Job::findOrFail($id)->delete();
+    // redirect
+    return redirect('/jobs');
+});
+
 Route::get("/contact", function () {
     return view("contact");
 });
