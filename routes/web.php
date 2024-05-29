936f92e3230d5ad292e3949a753a51d33b4a073c
thirsty-wing <112518380+thirsty-wing@users.noreply.github.com>
2024-05-28 23:15:55 -0600

    day 19: clean up the routes file
    Instead of doing a find on the id in every request, using the model type is a
    good way to go.
    
    running `php artisan route:list --except-vendor` will list all the routes of
    our app
    
    `Route::view` and `Route::resource` are some shortcuts to simply do a get ->
    view and register an entire controller respectively. Remember that there are
    conventional names that are used for the functions in a controller.

--- routes/web.php
+++ routes/web.php
@@ -1,77 +1,9 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
 
-Route::get("/", function () {
-    return view("home");
-});
+Route::view('/', 'home');
+Route::view("/contact", "contact");
 
-Route::get("/jobs", function () {
-    $jobs = Job::with('employer')->latest()->cursorPaginate(3);
-
-    return view("jobs.index", ["jobs" => $jobs]);
-});
-
-Route::get("/jobs/create", function () {
-    return view("jobs.create");
-});
-
-Route::get("/jobs/{id}", function ($id) {
-    $job = Job::find($id);
-
-    return view("jobs.show", ["job" => $job]);
-});
-
-Route::post("/jobs", function () {
-
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' =>  ['required'],
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1,
-    ]);
-
-    return redirect('/jobs');
-});
-
-Route::get("/jobs/{id}/edit", function ($id) {
-    $job = Job::find($id);
-
-    return view("jobs.edit", ["job" => $job]);
-});
-
-Route::patch("/jobs/{id}", function ($id) {
-    // validate
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' =>  ['required'],
-    ]);
-    // authorize (On hold...)
-    // update the job and persist
-    $job = Job::findOrFail($id);
-
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary'),
-    ]);
-
-    // redirect to the job page
-    return redirect('/jobs/' . $job->id);
-});
-
-Route::delete("/jobs/{id}", function ($id) {
-    // authorize (On hold...)
-    // delete the job
-    Job::findOrFail($id)->delete();
-    // redirect
-    return redirect('/jobs');
-});
-
-Route::get("/contact", function () {
-    return view("contact");
-});
+Route::resource('jobs', JobController::class);
