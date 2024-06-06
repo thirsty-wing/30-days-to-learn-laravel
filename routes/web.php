d047662c2eae1d7861166a66d951ac09ec9303d5
thirsty-wing <112518380+thirsty-wing@users.noreply.github.com>
2024-06-05 22:41:37 -0600

    day 25: add queues, jobs, and workers
    Users shouldn't need to wait for emails to be delivered. Instead of sending
    them synchronously, we can queue them up. The default queue uses the database,
    which is good enough for a while.
    
    When something is queued, it can be processed with `php artisan queue:work`.
    When something is queued, we need one or more workers to process those things
    on the queue.
    
    work can be queued with a delay to it's execution.
    
    A worker needs to be restarted if a job changes, or else the worker will take
    the out of date actions.

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,16 @@
 use App\Http\Controllers\SessionController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\JobController;
+use App\Models\Job;
 use Illuminate\Support\Facades\Route;
+
+Route::get('test', function () {
+    $job = Job::first();
+
+    \App\Jobs\TranslateJob::dispatch($job);
+
+    return 'done';
+});
 
 Route::view('/', 'home');
 Route::view("/contact", "contact");
